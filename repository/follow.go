e93fa4fef1e46b5013d15654d02267b43dffe4db
GoldenAmor <wfeng0221@gmail.com>
2022-06-11 22:03:59 +0800

    增加follows表结构体

--- repository/follow.go
+++ repository/follow.go
@@ -1,1 +1,15 @@
 package repository
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Follow struct {
+	Id        int64
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	UserId    int64
+	ToUserId  int64
+}
