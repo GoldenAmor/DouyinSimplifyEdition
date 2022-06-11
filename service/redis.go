13b5b1ea2d3582912cecfbecfcfd4549922825e6
GoldenAmor <wfeng0221@gmail.com>
2022-06-11 22:08:11 +0800

    实现Redis缓存层

--- service/redis.go
+++ service/redis.go
@@ -1,9 +1,29 @@
 package service
 
-import "github.com/RaymondCode/simple-demo/repository"
+import (
+	"encoding/json"
+	"github.com/RaymondCode/simple-demo/conn"
+	"github.com/RaymondCode/simple-demo/repository"
+	"time"
+)
 
 func GetUserByToken(token string) (*repository.User, error) {
-	return &repository.User{
-		ID: 1,
-	}, nil
+	users, err := conn.RedisDB.Get(token).Result()
+	if err != nil {
+		return nil, err
+	}
+	user := repository.User{}
+	err = json.Unmarshal([]byte(users), &user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func SaveUser2Redis(token string, userInfo string, expireTime time.Duration) error {
+	err := conn.RedisDB.Set(token, userInfo, expireTime).Err()
+	if err != nil {
+		return err
+	}
+	return nil
 }
