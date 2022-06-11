3f14157808f793d07f3ee4aea2fe662047320f6d
GoldenAmor <wfeng0221@gmail.com>
2022-06-11 22:08:52 +0800

    完善IsFollow逻辑

--- service/user.go
+++ service/user.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"fmt"
 	"github.com/RaymondCode/simple-demo/controller/vo"
 	"github.com/RaymondCode/simple-demo/dao"
 	"github.com/RaymondCode/simple-demo/repository"
@@ -32,8 +33,31 @@ func GetUserById(id int64) *repository.User {
 	return userDao.GetById(id)
 }
 
-func IsFollow(userId int64, targetId int64) bool {
-	return true
+func UpdateUserFollowCount(toUserId int64, count int64) error {
+	fmt.Println("更新")
+	fmt.Println(count)
+	userDao := dao.GetUserDaoInstance()
+	return userDao.UpdateUserFollowCount(toUserId, count)
+}
+
+func UpdateUserFollowedCount(byUserId int64, count int64) error {
+	fmt.Println("更新")
+	fmt.Println(count)
+	userDao := dao.GetUserDaoInstance()
+	return userDao.UpdateUserFollowedCount(byUserId, count)
+}
+
+func IsFollow(userId int64, targetId int64) (bool, error) {
+	followDao := dao.GetFollowDaoInstance()
+	record, _, err := followDao.GetFollowRecord(userId, targetId)
+	if err != nil {
+		return false, err
+	}
+	if record.RowsAffected == 0 {
+		return false, nil
+	} else {
+		return true, nil
+	}
 }
 
 func Transform2VoUser(user *repository.User) *vo.User {
